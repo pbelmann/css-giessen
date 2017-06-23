a75dc6f9395418c3ce08c46672db221de1834bfa
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2017-06-23 14:57:53 +0200

    add prepare_query script

---
+++ blast/prepare_query.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SWIFT_CSS=https://s3.computational.bio.uni-giessen.de/swift/v1/CSS
+
+if [ x!= x${1} ]; then
+	echo "usage: ${0} <targetdir" 1>2&
+       	exit 1
+fi;
+
+if [ ! -d  ${1} ]; then
+	mkdir -p ${1}
+fi
+
+cd ${1}
+curl ${SWIFt_CSS}/uniprot_sprot.fasta.gz > uniprot_sprot.fasta.gz
+gunzip uniprot_sprot.fasta.gz
+/vol/spool/css-giessen/scripts/split_fasta uniprot_sprot.fasta 5000
+
