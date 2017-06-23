5d6c5551657d5aa13d825a0ca0dd302b6eb880a9
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2017-06-23 13:01:41 +0200

    blast-demo -> add db creation

---
+++ blast/get_and_create_zebrafish_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+SWIFT_CSS=https://s3.computational.bio.uni-giessen.de/swift/v1/CSS
+if [ x = ${1}x ]; then
+	echo "usage : $0 <targetdir>" 1>&2
+	exit 1
+fi
+
+if [ ! -d ${1} ]; then
+	mkdir -p ${1}
+fi
+curl ${SWIFT_CSS}/zebrafish.1.protein.faa.gz > ${1}/zebrafish.1.protein.faa.gz
+gunzip zebrafish.1.protein.faa.gz
+docker run -v ${1}:/data makeblastdb -in zebrafish.1.protein.faa -dbtype prot
