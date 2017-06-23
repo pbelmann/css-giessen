32c9de0c6d438246f7310afc85bead798c3f0379
Jan Krueger <jkrueger@cebitec.uni-bielefeld.de>
2017-06-23 16:09:13 +0200

    fix

--- blast/blast.sh
+++ blast/blast.sh
@@ -2,10 +2,10 @@
 
 
 if [ x = ${1}x -o y = ${2}y -o z = ${3}z ]; then
-	echo "usage : ${0} <datadir> <dbdir> <prefix>" 1>&2
+	echo "usage : ${0} <sequencedir> <dbdir> <resultdir> <prefix>" 1>&2
 	exit 1;
 fi
 
 mkdir -p ${1}/results
 
-docker run -v ${1}:/data -v ${2}:/db biocontainers/blast blastp -query ${3}.${SGE_TASK_ID} -db /db/zebrafish.1.protein.faa -out results/result.txt.${SGE_TASK_ID}
+docker run -v ${1}:/input -v ${2}:/data  -v ${3}:/output biocontainers/blast blastp -query /input/${3}.${SGE_TASK_ID} -db zebrafish.1.protein.faa -out /output/result.txt.${SGE_TASK_ID}
